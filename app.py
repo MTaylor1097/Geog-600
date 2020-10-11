b4b2a5a8cdcb3a77bad83422ebc6b7e025893456
MTaylor1097 <72153216+MTaylor1097@users.noreply.github.com>
2020-10-11 17:37:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-#120 years of Olympic gold medal winners (summer games)
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
+import seaborn as sns
 
 data = pd.read_csv('athlete_events.csv')
 
-fig, ax = plt.subplots()
 
 #subset of summer games
 summer = data[data['Season']=='Summer']
@@ -25,51 +24,52 @@ f_group = SGF.groupby(["Year"])
 m_mean = m_group['Age'].apply(np.mean)
 f_mean = f_group['Age'].apply(np.mean)
 
+#mean of each year group so that x and y are the same size?
 m_meanx = m_group['Year'].apply(np.mean)
 f_meanx = f_group['Year'].apply(np.mean)
 
-#Plotting scatter
+# #Scatter
 
-Mx = m_meanx
-My = m_mean
+# Mx = m_meanx
+# My = m_mean
 
-Fx = f_meanx
-Fy = f_mean
+# Fx = f_meanx
+# Fy = f_mean
 
-plotM = ax.scatter(Mx, My, s=8,label =  "Male Athletes")
-plotF = ax.scatter(Fx, Fy, s= 8, label = "Female Athletes")
-plt.legend(loc = "upper right")
-ax.set_ylabel("Age")
-ax.set_xlabel("Olympic Year")
-ax.set_title("Age of Gold Medalists, 1896-2016")
-plt.savefig('Scatter.png')
+# plotM = plt.scatter(Mx, My, s=8,label =  "Male Athletes")
+# plotF = plt.scatter(Fx, Fy, s= 8, label = "Female Athletes")
+# plt.legend(loc = "upper right")
+# plt.title("Age of Gold Medalists, 1896-2016")
+# plt.xlabel("Olympic Year")
+# plt.ylabel("Age")
+# plt.savefig('Scatter.png')
+   
 
-# # Plot distributions of male and female gold medalists
-# # Need to remove missing data sets here as no NA values for Gold Medal winners age and when removed earlier removed values that can be plotted in scatter.
+#Histograms
 
-df = data.dropna()
-summer_df = df[df['Season']=='Summer']
-sum_gold_df = summer_df[summer_df["Medal"]== "Gold"]
-SGM_df = sum_gold_df[sum_gold_df["Sex"]=="M"]
-SGF_df = sum_gold_df[sum_gold_df["Sex"]=="F"]
-labels = ['Male', 'Female']
-width = 2
-
-
-m_age = SGM_df["Age"]
-f_age = SGF_df["Age"]
+# plt.hist(SGM['Age'], bins = 50, color = 'blue', label = 'Male', histtype = 'stepfilled', alpha = 0.3)
+# plt.hist(SGF['Age'], bins = 50, color = 'red', label = 'Female', histtype = 'stepfilled', alpha = 0.3);
+# plt.legend(loc = 'upper right')
+# plt.title("Gold Medal Age Distributions")
+# plt.xlabel("Age")
+# plt.ylabel("Frequency Density")
+# plt.savefig('Histograms.png')
 
 
-#plotting violin plot
-plot_data = [m_age, f_age]
-fig = plt.figure()
-plt.savefig('Violin.png')
-ax = fig.add_axes([1,1,1,1])
+# Time Series with two y axis
 
-# #bp is the box plot
-bp = ax.violinplot(plot_data, showmedians = True), (ax.set_ylabel('Age'), ax.set_title('Age Distribution of Gold Medal Winners'), ax.set_xticks([1,2]), ax.set_xticklabels(labels))
-plt.show()
-                         
+df_m = SGM.set_index('Year')
+df_f = SGF.set_index('Year')
+
+#df_m['Male Height Average'] = df_m['Height'].mean() 
+plt.plot(df_m.index, df_m['Height'], 'bo', markersize = 2, alpha = 0.3, label = 'Height (cm)')
+plt.xlabel('Olympic Year')
+plt.ylabel('Height(cm)')
+plt.legend(loc = 'upper center')
+plt.twinx()
+plt.plot(df_m.index+1, df_m['Weight'], 'ro', markersize = 2, alpha = 0.3, label = 'Weight (kg)')
+plt.ylabel('Weight (kg)')
+plt.legend()           
 
 
               
